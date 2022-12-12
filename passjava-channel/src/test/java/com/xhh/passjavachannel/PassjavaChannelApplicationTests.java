882fd2616dafb245bdf26d42022233cd4b1a45f3
xiaohuihui <2929836729@qq.com>
2022-12-12 15:19:06 +0800

    搭建初始项目框架完成

---
+++ passjava-channel/src/test/java/com/xhh/passjavachannel/PassjavaChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhh.passjavachannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaChannelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ passjava-content/src/test/java/com/xhh/passjava/passjavacontent/PassjavaContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhh.passjava.passjavacontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaContentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
